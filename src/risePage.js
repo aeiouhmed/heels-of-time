beb9142281c0760987e18a72b3f7ee8e924c9700
Alyssa <alyssaatmasava@gmail.com>
2025-07-24 13:47:22 +0800

    made responsive rise page

--- src/risePage.js
+++ src/risePage.js
@@ -16,12 +16,18 @@ function RisePage () {
           <img src="/risePage/heels.png" className="heels"/>
           <img src="/risePage/heels-2.svg" className="heel-2"/>
           <img src="/risePage/title.svg" className="title"/>
-          <img src="/risePage/note-1.svg" className="note-1"/>
-          <img src="/risePage/note-2.svg" className="note-2"/>
-          <img src="/risePage/note-3.svg" className="note-3"/>
-          <img src="/risePage/text-1.svg" className="text-1"/>
-          <img src="/risePage/text-2.svg" className="text-2"/>
-          <img src="/risePage/text-3.svg" className="text-3"/>
+          <div className="note note-1">
+            <img src="/risePage/note-1.svg" alt="" className="note-img" />
+            <img src="/risePage/text-1.svg" alt="" className="note-text-img" />
+          </div>
+          <div className="note note-2">
+            <img src="/risePage/note-2.svg" alt="" className="note-img" />
+            <img src="/risePage/text-2.svg" alt="" className="note-text-img" />
+          </div>
+          <div className="note note-3">
+            <img src="/risePage/note-3.svg" alt="" className="note-img" />
+            <img src="/risePage/text-3.svg" alt="" className="note-text-img" />
+          </div>
           <div className="go-back-btn">
             <button onClick={goBack}>GO BACK</button>
           </div>
